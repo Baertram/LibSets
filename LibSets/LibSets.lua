11be21679e3bf23c74372ac3668c2674743d516b
Baertram <pz1@gmx.net>
2019-04-15 20:44:16 +0200

    Fixed scanning packages to be 30 again
    Fixed scanning packages to be 30 again. Was lowered to 5 during tests in order to decrease waiting time

--- LibSets/LibSets.lua
+++ LibSets/LibSets.lua
@@ -370,8 +370,7 @@ function lib.LoadSets(override, fromAddonName)
     --> Change variable numItemIdPackages and increase it to support new added set itemIds
     --> Total itemIds collected: 0 to (numItemIdPackages * numItemIdPackageSize)
     local miliseconds = 0
-    --local numItemIdPackages = 30       -- Increase this to find new added set itemIds after and update
-local numItemIdPackages = 5       -- Increase this to find new added set itemIds after and update
+    local numItemIdPackages = 30       -- Increase this to find new added set itemIds after and update
     local numItemIdPackageSize = 5000  -- do not increase this or the client may crash!
     local fromTo = {}
     local fromVal = 0
