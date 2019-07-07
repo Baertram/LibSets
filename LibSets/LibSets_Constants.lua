5f128afbddfd5438bb880ed2e860cba7597e5cc8
Baertram <pz1@gmx.net>
2019-07-07 14:58:37 +0200

    Updated comments and debug description
    Updated comments and debug description

--- LibSets/LibSets_Constants.lua
+++ LibSets/LibSets_Constants.lua
@@ -14,12 +14,9 @@ lib.svVersion       = 0.6
 lib.setsLoaded      = false
 lib.setsScanning    = false
 
--- lookup this "max itemId scanned" for the current patch with the following script:
--- /script local maxId=147664 for itemId=maxId,250000 do local itemType = GetItemLinkItemType('|H1:item:'..tostring(itemId)..':30:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:10000:0|h|h') if itemType>0 then maxId=itemId end end d(maxId)
---lib.lastSetsPreloadedMaxItemId          = 152255 -- last checked: 2019-06-19, API 1000027
---The last checked API version for the setsData in file LibSets_Data.lua, see "local setItemIdsPreloaded = { ..."
--->Update here after a new scan of the maximum itemId was done (see above) AND the set item's itemIds were updated in file
--->LibSets_Data.lua, table setItemIdsPreloaded -> See description in this file above the table (data from WishList addon e.g.)
+--The last checked API version for the setsData in file LibSets_Data.lua, see table "lib.setDataPreloaded = { ..."
+-->Update here after a new scan of the set itemIds was done -> See LibSets_Data.lua, description in this file
+-->above the sub-table ["setItemIds"] (data from debug function LibSets.DebugScanAllSetData())
 lib.lastSetsPreloadedCheckAPIVersion    = 100027 --Elsweyr
 ------------------------------------------------------------------------------------------------------------------------
 --The supported languages of this library
