847454530dbeb69fe2f6ba0f32ff545757de486b
0815User <baertram.esoui@gmx.net>
2026-04-15 14:28:52 +0200

    Working on the PTS API101050

--- LibSets/Data/LibSets_Data_Sets.lua
+++ LibSets/Data/LibSets_Data_Sets.lua
@@ -31,7 +31,7 @@ local LIBSETS_SETTYPE_CLASS = LIBSETS_SETTYPE_CLASS
 
 
 ------------------------------------------------------------------------------------------------------------------------
---> Last updated: API 101049, 2026-02-23, Baertram
+--> Last updated: API 101050, 2026-04-15, Baertram
 ------------------------------------------------------------------------------------------------------------------------
 
 
@@ -145,7 +145,8 @@ local setInfo = {
 [850] = {wayshrines={616,616,616,}, dlcId=DLC_SEASON0	, zoneIds={1559,1559,1559}, veteran={[EQUIP_TYPE_HEAD]=true, [EQUIP_TYPE_SHOULDERS]=false}	, setType=LIBSETS_SETTYPE_MONSTER	, dropMechanic={LIBSETS_DROP_MECHANIC_NIGHT_MARKET}	}, --Thousand Eyes / Tausend Augen   [Night Market, Night Market, Night Market]
 [849] = {wayshrines={616,616,616,}, dlcId=DLC_SEASON0	, zoneIds={1559,1559,1559}, veteran={[EQUIP_TYPE_HEAD]=true, [EQUIP_TYPE_SHOULDERS]=false}	, setType=LIBSETS_SETTYPE_MONSTER	, dropMechanic={LIBSETS_DROP_MECHANIC_NIGHT_MARKET}	}, --Glittering Goad / Glanzvoller Ansporn   [Night Market, Night Market, Night Market]
 [851] = {wayshrines={616,616,616,}, dlcId=DLC_SEASON0	, zoneIds={1559,1559,1559}, veteran={[EQUIP_TYPE_HEAD]=true, [EQUIP_TYPE_SHOULDERS]=false}	, setType=LIBSETS_SETTYPE_MONSTER	, dropMechanic={LIBSETS_DROP_MECHANIC_NIGHT_MARKET}	}, --The Ruckus / Der Krawall   [Night Market, Night Market, Night Market]
-[855] = {wayshrines={-1,-1,-1,}, dlcId=DLC_SEASON0	, zoneIds={181,181,181,181,181,181}, veteran=false	, setType=LIBSETS_SETTYPE_CYRODIIL	, dropMechanic={LIBSETS_DROP_MECHANIC_MAIL_PVP_REWARDS_FOR_THE_WORTHY,LIBSETS_DROP_MECHANIC_CYRODIIL_BOARD_MISSIONS,LIBSETS_DROP_MECHANIC_AP_ELITE_GEAR_LOCKBOX_MERCHANT,LIBSETS_DROP_MECHANIC_CITY_CYRODIIL_CHEYDINHAL_CHORROL_WEYNON_PRIORY,LIBSETS_DROP_MECHANIC_OVERLAND_BOSS_DELVE,LIBSETS_DROP_MECHANIC_DOLMEN_HARROWSTORM_MAGICAL_ANOMALIES}	}, --Gorethief / Blutdieb   [Cyrodiil, Cyrodiil, Cyrodiil, Cyrodiil, Cyrodiil, Cyrodiil]
+[855] = {wayshrines={-1,-1,-1,}, dlcId=DLC_SEASON0	, zoneIds={181,181,181,181,181,181}, veteran=false	, setType=LIBSETS_SETTYPE_CYRODIIL	, dropMechanic={LIBSETS_DROP_MECHANIC_MAIL_PVP_REWARDS_FOR_THE_WORTHY,LIBSETS_DROP_MECHANIC_CYRODIIL_BOARD_MISSIONS,LIBSETS_DROP_MECHANIC_AP_ELITE_GEAR_LOCKBOX_MERCHANT,LIBSETS_DROP_MECHANIC_CITY_CYRODIIL_CHEYDINHAL_CHORROL_WEYNON_PRIORY,LIBSETS_DROP_MECHANIC_OVERLAND_BOSS_DELVE,LIBSETS_DROP_MECHANIC_DOLMEN_HARROWSTORM_MAGICAL_ANOMALIES}	}, --Gorethief / Blutdieb   [Cyrodiil, Cyrodiil, Cyrodiil, Cyrodiil, Cyrodiil, Cyrodiil],
+[854] = {wayshrines={-1,-1,-1,}, dlcId=DLC_SEASON0_PART2	, zoneIds={816}, veteran=false	, setType=LIBSETS_SETTYPE_MYTHIC	, dropMechanic={LIBSETS_DROP_MECHANIC_ZONE_STORYLINE}, dropMechanicDropLocationNames={['en']={'Thieves Guild zone storyline','','','','','',},['de']={'Diebes Gilde Zonen Geschichte','','','','','',},}	}, --The Prowler\'s Talisman / Vagabundentalisman   [Hew's Bane],
 } --lib.setInfo
 lib.setInfo = setInfo
 
